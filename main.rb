0d868f9d7d83d8ef0d30a43d21b06c6c34ad43eb
George <ubong_george@ymail.com>
2021-02-09 19:03:35 +0100

    Bubble sort by- first go

--- main.rb
+++ main.rb
@@ -19,6 +19,41 @@ def bubble_sort(arr)
   arr
 end
 
+def bubble_sort_by(arr)
+  swapped = true
+
+  while swapped == true
+    arr.each_with_index do |n, i|
+      next if arr[i + 1].nil?
+
+      swapped = false
+
+      yield(arr[i], arr[i + 1])
+
+      if n > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+  end
+
+  arr
+  
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+
+
 my_array = [5, 4, 2, 3, 8, 7]
 
+word_array = ['hi', 'hello', 'hey']
+
+bubble_sort_by(word_array) do |word|
+  puts word
+end
+
 p bubble_sort(my_array)
+
+
