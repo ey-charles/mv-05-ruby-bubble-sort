37ac3c69c33e6921a08a14915c04021a41387e8e
ey-charles <carlos.castillo@hey.com>
2021-02-09 12:22:11 -0500

    Use "next" instead of "unless"

--- main.rb
+++ main.rb
@@ -5,21 +5,20 @@ def bubble_sort(arr)
 
   while swapped == true
     arr.each_with_index do |n, i|
-      unless arr[i+1].nil?
-        swapped = false
-        if n > arr[i + 1]
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          swapped = true
-        end
+      next if arr[i + 1].nil?
+
+      swapped = false
+
+      if n > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
     end
   end
 
-  return arr
+  arr
 end
 
 my_array = [5, 4, 2, 3, 8, 7]
-
-# p my_array[6].nil?
 
 p bubble_sort(my_array)
