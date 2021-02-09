54b13554e60a76495fa19da7547729bff9acb622
ey-charles <carlos.castillo@hey.com>
2021-02-09 15:18:11 -0500

    Fix rubocop linter errors

--- main.rb
+++ main.rb
@@ -23,14 +23,14 @@ def bubble_sort_by(arr)
   swapped = true
 
   while swapped == true
-    arr.each_with_index do |n, i|
+    arr.each_index do |i|
       next if arr[i + 1].nil?
 
       swapped = false
 
       j = yield(arr[i], arr[i + 1])
 
-      if j > 0
+      if j.positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swapped = true
       end
@@ -39,15 +39,10 @@ def bubble_sort_by(arr)
   p arr
 end
 
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  left.length - right.length
-end
-
-
 my_array = [5, 4, 2, 3, 8, 7]
 
-word_array = ['hi', 'hello', 'hey']
+bubble_sort(my_array)
 
-p bubble_sort(my_array)
-
-
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
